f4be6da4553f0b18838d9e7da98e4534e778a377
João Mauricio <joaomauricio.dev@gmail.com>
2025-05-19 16:33:20 -0300

    fix: new arch

--- src/main/java/com/joaomauriciodev/observability_grafana/ObservabilityGrafanaApplication.java
+++ src/main/java/com/joaomauriciodev/observability_grafana/ObservabilityGrafanaApplication.java
@@ -1,13 +1,17 @@
 package com.joaomauriciodev.observability_grafana;
 
+import io.prometheus.client.exporter.HTTPServer;
+import io.prometheus.client.hotspot.DefaultExports;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.IOException;
 
 @SpringBootApplication
 public class ObservabilityGrafanaApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		SpringApplication.run(ObservabilityGrafanaApplication.class, args);
-	}
 
+	}
 }
