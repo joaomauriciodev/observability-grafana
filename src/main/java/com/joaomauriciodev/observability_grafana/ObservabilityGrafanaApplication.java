80888c0c98990b51bc491046c15a4d0690c01b69
João Mauricio <joaomauricio.dev@gmail.com>
2025-05-19 16:16:22 -0300

    initial commit

---
+++ src/main/java/com/joaomauriciodev/observability_grafana/ObservabilityGrafanaApplication.java
@@ -0,0 +1,13 @@
+package com.joaomauriciodev.observability_grafana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObservabilityGrafanaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ObservabilityGrafanaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/joaomauriciodev/observability_grafana/ObservabilityGrafanaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joaomauriciodev.observability_grafana;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObservabilityGrafanaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
