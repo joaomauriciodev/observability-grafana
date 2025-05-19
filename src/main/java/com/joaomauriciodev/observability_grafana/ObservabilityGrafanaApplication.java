7dae13b406b1ab501f5edcbba48738f080a40566
João Mauricio <joaomauricio.dev@gmail.com>
2025-05-19 17:30:17 -0300

    chore: create docker compose

--- src/main/java/com/joaomauriciodev/observability_grafana/ObservabilityGrafanaApplication.java
+++ src/main/java/com/joaomauriciodev/observability_grafana/ObservabilityGrafanaApplication.java
@@ -4,16 +4,33 @@ import io.prometheus.client.exporter.HTTPServer;
 import io.prometheus.client.hotspot.DefaultExports;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 import java.io.IOException;
 import java.util.Random;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class ObservabilityGrafanaApplication {
 
 	public static void main(String[] args) throws IOException {
 		SpringApplication.run(ObservabilityGrafanaApplication.class, args);
+		try {
 
+			MetricsConfig metricsConfig = new MetricsConfig();
+			metricsConfig.inicializarServidor(8080);
+
+			AppMetrics metrics = AppMetrics.getInstance();
+
+			metrics.definirUsuariosAtivos("administrador", 1);
+			metrics.definirUsuariosAtivos("comum", 10);
+
+			// Simula a aplicação em execução
+			simulaAplicacao(metrics);
+
+		} catch (IOException e) {
+			System.err.println("Erro ao iniciar servidor de métricas: " + e.getMessage());
+			e.printStackTrace();
+		}
 	}
 
 	private static void simulaAplicacao(AppMetrics metrics){
